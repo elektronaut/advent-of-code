eecf0040374d228f616003e0fea8a2153e0c746e
Inge Jørgensen <inge@anyone.no>
2020-12-07 23:08:58 +0100

    Day 5

---
+++ day05/day05.clj
@@ -0,0 +1,20 @@
+(require '[clojure.string :as str]
+         '[clojure.set :as set])
+
+(def passes
+  (str/split-lines (slurp "input.txt")))
+
+;; The input is just a binary number where F/L = 0 and B/R = 1,
+;; there's no need to split the string and calculate column/row.
+(defn seat-id [s]
+  (Integer/parseInt
+   (str/replace (str/replace s #"[FL]" "0") #"[BR]" "1") 2))
+
+(println "Part 1:" (reduce max (map seat-id passes)))
+
+(let [taken-seats (map seat-id passes)
+      seats (range (reduce min taken-seats)
+                   (reduce max taken-seats))
+      available-seats (set/difference (set seats)
+                                      (set taken-seats))]
+  (println "Part 2:" (first available-seats)))
