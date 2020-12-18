0a21eff46291bc6c772a16da695cddc98a4b215a
Inge Jørgensen <inge@anyone.no>
2020-12-18 10:27:24 +0100

    Day 18

---
+++ day18/day18.clj
@@ -0,0 +1,19 @@
+(require '[clojure.string :as str])
+
+(defn prefix [pre items]
+  (cons pre (map #(if (list? %) (prefix pre %) %) items)))
+
+(defn calc-no-precedence [init & ops]
+  (reduce (fn [v [op n]] (apply op [v n])) init (partition 2 ops)))
+
+(defn calc-precedence [& items]
+  (->> (partition-by #(= * %) items)
+       (map #(filter number? %))
+       (filter #(not (empty? %)))
+       (map #(apply + %))
+       (reduce *)))
+
+(let [sexps (->> (str/split-lines (slurp "input.txt"))
+                 (map #(read-string (str "(" % ")"))))]
+  (println "Part 1:" (reduce + (map #(eval (prefix 'calc-no-precedence %)) sexps)))
+  (println "Part 2:" (reduce + (map #(eval (prefix 'calc-precedence %)) sexps))))
