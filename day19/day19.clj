d4c81bb7908bd6758373cfe328c72b0306fbe910
Inge Jørgensen <inge@anyone.no>
2020-12-21 00:52:55 +0100

    Day 19

---
+++ day19/day19.clj
@@ -0,0 +1,32 @@
+(require '[clojure.string :as str])
+
+(defn parse-rule [s]
+  (let [[key values] (str/split s #": ")
+        rules (->> (str/split values #" \| ")
+                   (map #(read-string (str "[" % "]"))))]
+    {(Integer/parseInt key) rules}))
+
+(defn test-message [rules r message]
+  (mapcat
+   (fn [group]
+     (reduce
+      (fn [strs rule]
+        (if (string? rule)
+          (map #(apply str (rest %)) (filter #(= rule (str (first %))) strs))
+          (mapcat #(test-message rules rule %) strs))
+        ) [message] group))
+   (rules r)))
+
+(defn matches? [rules message]
+  (let [result (first (test-message rules 0 message))]
+    (and (empty? result)
+         (string? result))))
+
+(let [blocks (map str/split-lines (str/split (slurp "input.txt") #"\n\n"))
+      rules (into {} (map parse-rule (first blocks)))
+      messages (last blocks)
+      new-rules {8 '([42] [42 8]) 11 '([42 31] [42 11 31])}]
+  (println "Part 1:"
+           (count (filter #(matches? rules %) messages)))
+  (println "Part 2:"
+           (count (filter #(matches? (merge rules new-rules) %) messages))))
