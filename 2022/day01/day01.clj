262858be0b8a4365cabfc95b2c3d768e5f10cb00
Inge Jørgensen <inge@elektronaut.no>
2022-12-01 22:45:10 +0100

    Slightly more elegant map

--- 2022/day01/day01.clj
+++ 2022/day01/day01.clj
@@ -1,9 +1,12 @@
 (require '[clojure.string :as str])
 
+(defn int [s]
+  (Integer/parseInt s))
+
 (def elves
   (->> (str/split (slurp "input.txt") #"\n\n")
        (map str/split-lines)
-       (map (fn [lines] (reduce + (map #(Integer/parseInt %) lines))))
+       (map #(reduce + (map int %)))
        (sort #(compare %2 %1))))
 
 (println "Part 1:" (first elves))
