4a86f496d98a416fe98c8630874871d61067c780
Inge Jørgensen <inge@elektronaut.no>
2022-12-03 00:45:03 +0100

    Day 2

---
+++ 2022/day02/day02.clj
@@ -0,0 +1,30 @@
+(require '[clojure.string :as str])
+
+(def plays (->> (slurp "input.txt")
+                (str/split-lines)
+                (map #(str/split % #" "))))
+
+(def shapes {"A" 1 "B" 2 "C" 3
+             "X" 1 "Y" 2 "Z" 3})
+
+(defn outcome-points [a b]
+  ([3 0 6]
+   (mod (- (+ 3 (shapes a)) (shapes b)) 3)))
+
+(defn score [[a b]]
+  (+ (shapes b)
+     (outcome-points a b)))
+
+(defn opposing-shape [opponent strategy]
+  (let [offset ({"X" -1 "Y" 0 "Z" 1} strategy)]
+    (nth (cycle ["C" "A" "B"])
+         (+ (shapes opponent) offset))))
+
+(defn opposing-play [[a b]]
+  [a (opposing-shape a b)])
+
+(println "Part 1:" (reduce + (map score plays)))
+
+(println "Part 1:" (->> (map opposing-play plays)
+                        (map score)
+                        (reduce +)))
