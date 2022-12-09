a8bb7bbadc8167ebb1df0d532493317705bb7f0d
Inge Jørgensen <inge@elektronaut.no>
2022-12-09 01:13:21 +0100

    Day 8, part 2

--- 2022/day08/day08.clj
+++ 2022/day08/day08.clj
@@ -8,10 +8,15 @@
        (map (fn [line] (vec (map #(Integer/parseInt %) (str/split line #"")))))
        (vec)))
 
+(defn coordinates [grid]
+  (for [x (range 0 (count (nth grid 0)))
+        y (range 0 (count grid))]
+    [x y]))
+
 (defn sight-lines [[x y]]
   (let* [row (nth trees y)
          column (vec (map #(nth % x) trees))]
-    [(subvec column 0 y) (subvec row 0 x)
+    [(reverse (subvec column 0 y)) (reverse (subvec row 0 x))
      (subvec column (inc y)) (subvec row (inc x))]))
 
 (defn visible [[x y]]
@@ -21,11 +26,22 @@
          (filter empty?)
          (seq))))
 
-(defn coordinates [grid]
-  (for [x (range 0 (count (nth grid 0)))
-        y (range 0 (count grid))]
-    [x y]))
+(defn view-distance [max coll]
+  (let [visible-tree (first (filter #(>= % max) coll))]
+    (if visible-tree
+      (inc (.indexOf coll visible-tree))
+      (count coll))))
+
+(defn scenic-score [[x y]]
+  (let [height (get-in trees [y x])]
+    (->> (sight-lines [x y])
+         (map (partial view-distance height))
+         (reduce *))))
 
 (println "Part 1:" (->> (coordinates trees)
                         (filter visible)
                         (count)))
+
+(println "Part 2:" (->> (coordinates trees)
+                        (map scenic-score)
+                        (reduce max)))
