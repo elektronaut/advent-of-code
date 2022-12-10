34b38df2c9ab6e04983a5bad3c7c1f6e6d152465
Inge Jørgensen <inge@elektronaut.no>
2022-12-10 16:59:35 +0100

    Day 10, part 2

--- 2022/day10/day10.clj
+++ 2022/day10/day10.clj
@@ -24,6 +24,18 @@
 (defn signal-strength [cycle]
   (* cycle (nth cycles (dec cycle))))
 
+(defn pixel [i x]
+  (if (and (>= i (dec x))
+           (<= i (inc x)))
+    "#"
+    "."))
+
 (println "Part 1:" (->> [20 60 100 140 180 220]
                         (map signal-strength)
                         (reduce +)))
+
+(do (println "Part 2:")
+    (->> (partition 40 cycles)
+         (map #(map-indexed pixel %))
+         (map str/join)
+         (map println)))
