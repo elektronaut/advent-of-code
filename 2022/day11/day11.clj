937659202ea3ab6b77aa39cc96b8c962cbbf848b
Inge Jørgensen <inge@elektronaut.no>
2022-12-11 23:35:03 +0100

    Day 11, part 1

--- 2022/day11/day11.clj
+++ 2022/day11/day11.clj
@@ -20,4 +20,34 @@
 
 (def monkeys
   (->> (str/split (slurp "2022/day11/input.txt") #"\n\n")
-       (map parse-monkey)))
+       (map parse-monkey)
+       (vec)))
+
+(defn process-item [monkey monkeys item]
+  (let [worry-level (quot ((monkey :operation) item) 3)
+        target (if (= 0 (mod worry-level (monkey :test)))
+                 (monkey :if-true)
+                 (monkey :if-false))
+        target-monkey (nth monkeys target)
+        target-items (conj (target-monkey :items) worry-level)]
+    (assoc monkeys target (assoc target-monkey :items target-items))))
+
+(defn process-monkey [monkeys index]
+  (let [monkey (nth monkeys index)
+        items (monkey :items)
+        next-monkeys (reduce (partial process-item monkey) monkeys items)]
+    (assoc next-monkeys index
+           (-> monkey
+               (assoc :items [])
+               (assoc :inspect-count (+ (count items) (monkey :inspect-count)))))))
+
+(defn round [monkeys]
+  (reduce process-monkey monkeys (range 0 (count monkeys))))
+
+(println "Part 1:"
+         (->> (nth (iterate round monkeys) 20)
+              (sort-by :inspect-count)
+              (reverse)
+              (take 2)
+              (map :inspect-count)
+              (reduce *)))
